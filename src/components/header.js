a09ba7ca4f929a0db56e99c8a7466ac9feecfa7b
sperkn <nicholas.kreps@gmail.com>
2018-11-18 12:53:24 -0500

    set up project for sass/scss & added initial sass files/variables

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const Header = () => (
+  // render() {
+  //   return (
+      <div>
+        <h2>Inventory</h2>
+        <button>
+          Load Sample Fishes
+        </button>
+      </div>
+  //   );
+  // }
 )
 
 export default Header
