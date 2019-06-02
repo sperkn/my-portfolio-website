ffb685e3637440a581b885231dd2d6ca4e718e05
sperkn <nicholas.kreps@gmail.com>
2019-06-01 23:58:53 -0400

    removed images and commented out icon image in config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: '#48B49C',
         theme_color: '##48B49C',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
