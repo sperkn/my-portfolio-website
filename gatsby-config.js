eb6640f9cfdc06fe15e2f6c7ab3edcb0156298c4
sperkn <nicholas.kreps@gmail.com>
2019-06-06 12:09:53 -0400

    dependency and config updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Nicholas Kreps - Web Developer, Engineer',
+    title: 'Nicholas Kreps',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,13 +16,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'sperkn',
-        short_name: 'starter',
+        name: 'Nicholas Kreps',
+        short_name: 'Nicholas Kreps',
         start_url: '/',
-        background_color: '#48B49C',
-        theme_color: '##48B49C',
+        background_color: 'rgb(68, 108, 139)',
+        theme_color: 'rgb(68, 108, 139)',
         display: 'minimal-ui',
         // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Source Sans Pro`,
+            variants: [`300`, `400`, `700`, `900`]
+          },
+        ],
       },
     },
     `gatsby-plugin-sass`,
