9689c303ec5d35a0ad191a9d9fcd06c0e1247fca
sperkn <nicholas.kreps@gmail.com>
2019-06-11 21:52:22 -0400

    moved description list inside the link so that it is easier for users to get to about page, updated associated styling to get description titles centered and removed margin on bottom so that on hover the shadow is not exagerated

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ class Header extends Component {
       <header className="header">
         <Link to="/about" className="header__link">
           <h1 className="header__name">Nicholas Kreps</h1>
+          <ul className="header__description"><li className="header__titles">Engineer</li>  •  <li className="header__titles">Code</li> •  <li className="header__titles">Create</li></ul>
         </Link>
-        <ul className="header__description"><li className="header__titles">Engineer</li>  •  <li className="header__titles">Code</li> •  <li className="header__titles">Create</li></ul>
         <Navbar />
       </header>
     )
