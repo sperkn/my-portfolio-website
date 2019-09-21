d3ba3fdcff22b196a8d81cb94399afb57e98ce6e
sperkn <nicholas.kreps@gmail.com>
2019-09-21 18:48:45 -0400

    updated config with site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: 'Nicholas Kreps',
+    titleTemplate: '%s · Nicholas Kreps',
+    description: 'Engineer, Developer & Creator',
+    url: 'https://www.nicholaskreps.com',
+    image: '/images/headshot.jpg',
+    owner: 'Nicholas Kreps',
+    twitter: '@nicholaskreps',
+    nav: [
+      { path: '/about', name: 'About' },
+      { path: '/contact', name: 'Contact' },
+    ],
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -22,7 +32,7 @@ module.exports = {
         background_color: 'rgb(68, 108, 139)',
         theme_color: 'rgb(68, 108, 139)',
         display: 'minimal-ui',
-        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/headshot-logo.png', // This path is relative to the root of the site.
       },
     },
     {
